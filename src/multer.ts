98350316005db25a0417eb378dc24280a58c0cb0
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2025-02-12 15:53:52 +0600

    add types in multer config

--- src/multer.ts
+++ src/multer.ts
@@ -1,17 +1,18 @@
-import multer from "multer";
+import multer, { StorageEngine } from "multer";
 import { promises as fs } from "fs";
 import path from "path";
 import { randomUUID } from "crypto";
+import { Request } from "express";
 
 const publicPath = path.join(__dirname, "assets");
 
-const imageStorage = multer.diskStorage({
-  destination: async (req, file, cb) => {
+const imageStorage: StorageEngine = multer.diskStorage({
+  destination: async (req: Request, file: Express.Multer.File, cb) => {
     const destDir = path.join(publicPath, "public");
     await fs.mkdir(destDir, { recursive: true });
     cb(null, publicPath);
   },
-  filename: (req, file, cb) => {
+  filename: (req: Request, file: Express.Multer.File, cb) => {
     const extension = path.extname(file.originalname);
     const filename = path.join("public", randomUUID() + extension);
     cb(null, filename);
